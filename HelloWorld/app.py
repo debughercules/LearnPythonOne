60f9e02528f6846f937b8b4aae099aa728025c18
Bharat Byan <>
2024-01-22 18:40:35 +0100

    First Commit
    Python Learn Basics
    https://www.youtube.com/watch?v=kqtD5dpn9C8

---
+++ HelloWorld/app.py
@@ -0,0 +1,29 @@
+#1 variables
+# age = 20
+# price = 19.5
+# first_name = "Mosh"
+# is_online = False
+# print(price)
+
+
+#2 input functions
+# name = input("Whar is your name? ")
+# print("Hello " + name)
+
+
+#3 type conversion
+# birth_year = input("Enter your birth year: ")
+# age = 2024 - int(birth_year)
+# print(age)
+
+
+
+
+# String functions
+# course = "Python for Beginners"
+# print(course.upper())
+
+# arithmatic operators
+x = 10
+x += 3
+print(x)
